5ac52d8357fafba4c148b5ee0e79b2a03b7fdf5c
musty4real2 <musty4real2@gmail.com>
2024-01-21 00:47:18 +0100

    additions

---
+++ test.js
@@ -0,0 +1,53 @@
+// document.getElementById("count-el").innerText = 5
+let count = 0;
+
+console.log(count);
+
+
+// 1. Create a variable, myAge, and set its value to your age
+
+// 2. Log the myAge variable to the console
+
+// let myAge=32;
+
+// console.log(myAge);
+
+
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+// let myAge = 32;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+
+// console.log(myDogAge);
+
+// let bonusPoints = 50
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints);
+
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints);
+
+
+// let lapsCompleted = 0
+
+// // Create a function that increments the lapsCompleted variable with one
+// // Run it three times
+
+// function incrementLap() {
+//     lapsCompleted = lapsCompleted + 1;
+// }
+
+// incrementLap();
+// incrementLap();
+// incrementLap();
+
+// console.log(lapsCompleted);
