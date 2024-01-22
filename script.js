530622aefe90b3331bffe4c6dd0095d42d1166be
musty4real2 <musty4real2@gmail.com>
2024-01-22 13:07:07 +0100

    passenger counter

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
+// let saveEl = document.getElementById("save-el");
+// let countEl = document.getElementById("count-el");
+// let count = 0;
+
+// console.log(saveEl);
+
+// function increment() {
+//     count += 1;
+//     countEl.innerText = count;
+// }
+
+// function save() {
+//     let countStr = count + " - ";
+//     saveEl.innerText += countStr;
+//     console.log(count);
+// }
+
+
+
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 let count = 0;
 
-console.log(saveEl);
-
 function increment() {
     count += 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 
 function save() {
     let countStr = count + " - ";
-    saveEl.innerText += countStr;
-    console.log(count);
+    saveEl.textContent += countStr;
 }
